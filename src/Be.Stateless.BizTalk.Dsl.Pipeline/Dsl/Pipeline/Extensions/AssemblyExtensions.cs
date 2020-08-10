4a203951173fb388e1b59649675592bfae75ec1d
François Chabot <fchabot@skynet.be>
2020-08-10 09:59:21 +0200

    Add GetPipelineDefinitionTypes for better component SRP

---
+++ src/Be.Stateless.BizTalk.Dsl.Pipeline/Dsl/Pipeline/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,61 @@
+﻿#region Copyright & License
+
+// Copyright © 2012 - 2020 François Chabot
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace Be.Stateless.BizTalk.Dsl.Pipeline.Extensions
+{
+	[SuppressMessage("ReSharper", "UnusedType.Global", Justification = "Public API.")]
+	[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Public API.")]
+	[SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "Public API.")]
+	public static class AssemblyExtensions
+	{
+		public static Type[] GetPipelineDefinitionTypes(this IEnumerable<string> assemblyPaths)
+		{
+			if (assemblyPaths == null) throw new ArgumentNullException(nameof(assemblyPaths));
+			return assemblyPaths.Select(Assembly.LoadFrom)
+				// make sure all assemblies are loaded before proceeding with reflection
+				.ToArray()
+				.GetPipelineDefinitionTypes();
+		}
+
+		public static Type[] GetPipelineDefinitionTypes(this IEnumerable<Assembly> assemblies)
+		{
+			if (assemblies == null) throw new ArgumentNullException(nameof(assemblies));
+			return assemblies
+				// discard this assembly, i.e. Be.Stateless.BizTalk.Dsl.Pipeline
+				.Where(a => a != Assembly.GetExecutingAssembly())
+				.SelectMany(a => a.GetPipelineDefinitionTypes())
+				.ToArray();
+		}
+
+		[SuppressMessage("ReSharper", "ReturnTypeCanBeEnumerable.Global")]
+		public static Type[] GetPipelineDefinitionTypes(this Assembly assembly)
+		{
+			if (assembly == null) throw new ArgumentNullException(nameof(assembly));
+			return assembly.GetTypes()
+				.Where(t => t.IsConcrete())
+				.Where(t => t.IsPipelineDefinition())
+				.ToArray();
+		}
+	}
+}
--- src/Be.Stateless.BizTalk.Dsl.Pipeline/Dsl/Pipeline/Extensions/TypeExtensions.cs
+++ src/Be.Stateless.BizTalk.Dsl.Pipeline/Dsl/Pipeline/Extensions/TypeExtensions.cs
@@ -17,30 +17,54 @@
 #endregion
 
 using System;
+using System.Reflection;
 
 namespace Be.Stateless.BizTalk.Dsl.Pipeline.Extensions
 {
 	internal static class TypeExtensions
 	{
-		internal static ReceivePipeline AsReceivePipeline(this Type pipelineType)
+		internal static ReceivePipeline AsReceivePipeline(this Type type)
 		{
-			if (pipelineType == null) throw new ArgumentNullException(nameof(pipelineType));
-			if (typeof(ReceivePipeline).IsAssignableFrom(pipelineType)) return (ReceivePipeline) Activator.CreateInstance(pipelineType);
-			if (typeof(SendPipeline).IsAssignableFrom(pipelineType)) return null;
-			throw new ArgumentException(BuildMessage(pipelineType));
+			if (type == null) throw new ArgumentNullException(nameof(type));
+			if (type.IsReceivePipelineDefinition()) return (ReceivePipeline) Activator.CreateInstance(type);
+			if (type.IsSendPipelineDefinition()) return null;
+			throw new ArgumentException(BuildMessage(type));
 		}
 
-		internal static SendPipeline AsSendPipeline(this Type pipelineType)
+		internal static SendPipeline AsSendPipeline(this Type type)
 		{
-			if (pipelineType == null) throw new ArgumentNullException(nameof(pipelineType));
-			if (typeof(ReceivePipeline).IsAssignableFrom(pipelineType)) return null;
-			if (typeof(SendPipeline).IsAssignableFrom(pipelineType)) return (SendPipeline) Activator.CreateInstance(pipelineType);
-			throw new ArgumentException(BuildMessage(pipelineType));
+			if (type == null) throw new ArgumentNullException(nameof(type));
+			if (type.IsReceivePipelineDefinition()) return null;
+			if (type.IsSendPipelineDefinition()) return (SendPipeline) Activator.CreateInstance(type);
+			throw new ArgumentException(BuildMessage(type));
 		}
 
-		private static string BuildMessage(Type pipelineType)
+		internal static bool IsPipelineDefinition(this Type type)
 		{
-			return $"'{pipelineType.FullName}' does not derive from neither '{typeof(ReceivePipeline).FullName}' nor '{typeof(SendPipeline).FullName}'.";
+			return type.IsReceivePipelineDefinition() || type.IsSendPipelineDefinition();
 		}
+
+		private static bool IsReceivePipelineDefinition(this Type type)
+		{
+			return type.IsConcrete() && _dslBaseReceivePipelineType.IsAssignableFrom(type.GetTypeInfo());
+		}
+
+		private static bool IsSendPipelineDefinition(this Type type)
+		{
+			return type.IsConcrete() && _dslBaseSendPipelineType.IsAssignableFrom(type.GetTypeInfo());
+		}
+
+		internal static bool IsConcrete(this Type type)
+		{
+			return !type.GetTypeInfo().IsAbstract && !type.GetTypeInfo().IsGenericTypeDefinition;
+		}
+
+		private static string BuildMessage(Type type)
+		{
+			return $"'{type.FullName}' does not derive from neither '{typeof(ReceivePipeline).FullName}' nor '{typeof(SendPipeline).FullName}'.";
+		}
+
+		private static readonly TypeInfo _dslBaseReceivePipelineType = typeof(ReceivePipeline).GetTypeInfo();
+		private static readonly TypeInfo _dslBaseSendPipelineType = typeof(SendPipeline).GetTypeInfo();
 	}
 }
